5da55dd36d4c43673d9d989f16124ed3d0adcf96
Abhijithkp2023 <abhijithkp642@gmail.com>
2024-03-25 14:26:31 +0530

    CRA & tailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+    <div className="font-bold text-2xl text-green-800 --4 m-4 text-center">
+     Hello world, we are building netflix gpt
     </div>
   );
 }
