d259d3b40a0be66f54e6088a29f4156ab3cc71d2
Abhijithkp2023 <abhijithkp642@gmail.com>
2024-04-01 22:32:30 +0530

    Add Upcoming Movies hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -5,11 +5,13 @@ import SecondayContainer from "./SecondaryContainer"
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import usePopularMovies from '../hooks/usePopularMovies';
 import useTopRatedMovies from '../hooks/useTopRatedMovies';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
 
 const Browse = () => {
 useNowPlayingMovies();
 usePopularMovies();
 useTopRatedMovies();
+useUpcomingMovies();
 
   return (
     <div>
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,10 +8,10 @@ const SecondaryContainer = () => {
     movies.nowPlayingMovies && (
     <div className='bg-black'>
       <div className='-mt-60 pl-12 z-20 relative'>
+      <MovieList title={"Upcoming Movies"} movies = {movies.upcomingMovies} />
       <MovieList title={"Now Playing Movies"} movies = {movies.nowPlayingMovies} />
       <MovieList title={"Top Rated Movies"} movies = {movies.topRatedMovies} />
       <MovieList title={"Popular Movies"} movies = {movies.popularMovies} />
-      
       </div>
     </div>
     )
